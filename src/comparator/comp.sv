3291ca039adf0f0bd610db423c4bb501a51aeb87
AdKm-2507 <ee250002007@iiti.ac.in>
2026-06-17 14:04:45 +0530

    2 output signals now, only lt uses signed signal

--- src/comparator/comp.sv
+++ src/comparator/comp.sv
@@ -1,15 +1,13 @@
 `timescale 1ns / 1ps
 
 module comparator(
-    input signed [7:0] r1,
-    input signed [7:0] r2,
+    input [7:0] r1,
+    input [7:0] r2,
     output wire eq,
-    output wire lt,
-    output wire ge
+    output wire lt
 );
 
 assign eq = (r1 == r2);
-assign lt = (r1 < r2);
-assign ge = (r1 >= r2);
+assign lt = ($signed(r1) < $signed(r2));
 
 endmodule
