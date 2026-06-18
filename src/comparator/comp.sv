55a624c891cba1368cbbc1dc98f25d3eb4480544
AdKm-2507 <ee250002007@iiti.ac.in>
2026-06-18 19:41:00 +0530

    r -> readData to match regfile output

--- src/comparator/comp.sv
+++ src/comparator/comp.sv
@@ -1,13 +1,13 @@
 `timescale 1ns / 1ps
 
 module comparator(
-    input [7:0] r1,
-    input [7:0] r2,
+    input [7:0] readData1,
+    input [7:0] readData2,
     output wire eq,
     output wire lt
 );
 
-assign eq = (r1 == r2);
-assign lt = ($signed(r1) < $signed(r2));
+assign eq = (readData1 == readData2);
+assign lt = ($signed(readData1) < $signed(readData2));
 
 endmodule
