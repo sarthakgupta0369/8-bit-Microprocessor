fce7be17cc79fad51737252e51f5aa0fafcf6dc2
AdKm-2507 <ee250002007@iiti.ac.in>
2026-06-19 03:08:57 +0530

    naming regularity

--- src/RAM/ram.sv
+++ src/RAM/ram.sv
@@ -2,22 +2,20 @@
 
 module ram(
   input clk,
-  input memWrite,
-  input memRead, 
+  input memWrite, 
+  input memRead,
   input [7:0] address,
-  input [7:0] writeData,
-  output [7:0] out
+  input [7:0] writeMem,
+  output [7:0] readMem
 );
   
   reg[7:0] mem [255:0];
   
   always @ (posedge clk) begin
     if(memWrite) begin
-      mem[address] <=writeData;
+      mem[address] <=writeMem;
     end
   end
   
-  assign out = memRead ? mem[address] : 8'b0; 
-  
-    
+  assign readMem = memRead ? mem[address] : 8'b0;
 endmodule
