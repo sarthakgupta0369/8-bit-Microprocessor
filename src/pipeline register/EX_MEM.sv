fb5d812d396b1c3e72f84e28da91a636b049c99e
AdKm-2507 <ee250002007@iiti.ac.in>
2026-07-13 23:31:17 +0530

    changes in input

--- src/pipeline register/EX_MEM.sv
+++ src/pipeline register/EX_MEM.sv
@@ -5,7 +5,7 @@ module EX_MEM(
     input              reset,
     
     //data
-    input wire  [7:0]  readData2E,
+    input wire  [7:0]  operandBE,
     input wire  [2:0]  writeRegE, 
     input wire  [7:0]  aluResultE,
     
@@ -17,7 +17,7 @@ module EX_MEM(
     input wire         memReadE,
 
     //data
-    output reg  [7:0]  readData2M,
+    output reg  [7:0]  operandBM,
     output reg  [2:0]  writeRegM, 
     output reg  [7:0]  aluResultM,
     
@@ -31,24 +31,24 @@ module EX_MEM(
 
     always @(posedge clk) begin
         if (~reset) begin
-            readData2M <= readData2E;
-            writeRegM  <= writeRegE;
-            aluResultM <= aluResultE;
-            regWriteM  <= regWriteE;
-            imm8M      <= imm8E;
-            regSrcM    <= regSrcE;
-            memWriteM  <= memWriteE;
-            memReadM   <= memReadE;
+            operandBM    <= operandBE;
+            writeRegM    <= writeRegE;
+            aluResultM   <= aluResultE;
+            regWriteM    <= regWriteE;
+            imm8M        <= imm8E;
+            regSrcM      <= regSrcE;
+            memWriteM    <= memWriteE;
+            memReadM     <= memReadE;
         end 
         else begin 
-            readData2M <= 8'd0;
-            writeRegM  <= 3'd0;
-            aluResultM <= 8'd0;
-            regWriteM  <= 1'b0;
-            imm8M      <= 8'd0;
-            regSrcM    <= 2'd0;
-            memWriteM  <= 1'b0;
-            memReadM   <= 1'b0;  
+            operandBM    <= 8'd0;
+            writeRegM    <= 3'd0;
+            aluResultM   <= 8'd0;
+            regWriteM    <= 1'b0;
+            imm8M        <= 8'd0;
+            regSrcM      <= 2'd0;
+            memWriteM    <= 1'b0;
+            memReadM     <= 1'b0;  
         end
     end        
 endmodule
