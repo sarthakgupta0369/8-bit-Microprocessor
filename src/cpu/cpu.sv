b8405408c325d58c187d89a6d979dcbf7b26a926
Sarthak Gupta <sarthakgupta0369@gmail.com>
2026-07-13 23:27:29 +0530

    Implemented cpu for pipeling and Integrated cpu with hazard unit

--- src/cpu/cpu.sv
+++ src/cpu/cpu.sv
@@ -12,12 +12,14 @@ module cpu (
     
     wire [15:0] target_addressE;
     wire        is_branchE;
-
+    wire        pcSrc; // added pcSrc wire
+    wire        flushIFID;
+    wire        stall;
 
     ProgramCounter pc_inst (
         .clk      (clk),
         .rst      (reset),
-        .pc_write (1'b1),
+        .pc_write (~stall),
         .pc_next  (pcNext),
         .pc       (pcCurrent)
     );
@@ -35,12 +37,14 @@ module cpu (
     wire [15:0] instructionD;
 
     IF_ID if_id (
-        .clk           (clk),
-        .reset         (reset),
+        .clk          (clk),
+        .reset        (reset),
+        .clr          (flushIFID),
+        .en           (~stall),
         .pcPlus1I     (pcPlus1),
-        .instructionI  (instructionF),
+        .instructionI (instructionF),
         .pcPlus1D     (pcPlus1D),
-        .instructionD  (instructionD)
+        .instructionD (instructionD)
     );
 
 
@@ -99,11 +103,23 @@ module cpu (
         .readData1 (readData1D),
         .readData2 (readData2D)
     );
+    
+    // EX/MEM
+    wire [7:0]  operandBM;
+    wire [2:0]  writeRegM;
+    wire [7:0]  aluResultM;
+    wire        regWriteM; 
+    wire [7:0]  imm8M;
+    wire [1:0]  regSrcM;
+    wire        memWriteM;
+    wire        memReadM;
 
  //ID/ EX
     wire [15:0] pcPlus1E;
     wire [7:0]  readData1E;
     wire [7:0]  readData2E;
+    wire [2:0]  readReg1E;
+    wire [2:0]  readReg2E;
     wire [2:0]  writeRegE;
     wire        regWriteE;
     wire [3:0]  aluControlE;
@@ -116,14 +132,18 @@ module cpu (
     wire [1:0]  branchControlE;
     wire        branchE;
     wire        jumpE;
+    wire        flushIDEX;
 
     ID_EX id_ex (
         .clk            (clk),
         .reset          (reset),
+        .clr            (flushIDEX),
         .pcPlus1D       (pcPlus1D),
         .readData1D     (readData1D),
         .readData2D     (readData2D),
         .writeRegD      (writeRegD),
+        .readReg1D      (readReg1D),
+        .readReg2D      (readReg2D),
         .regWriteD      (regWriteD),
         .aluControlD    (aluControlD),
         .imm8D          (imm8D),
@@ -139,6 +159,8 @@ module cpu (
         .readData1E     (readData1E),
         .readData2E     (readData2E),
         .writeRegE      (writeRegE),
+        .readReg1E      (readReg1E),
+        .readReg2E      (readReg2E),
         .regWriteE      (regWriteE),
         .aluControlE    (aluControlE),
         .imm8E          (imm8E),
@@ -153,14 +175,26 @@ module cpu (
     );
 
     // alu
+    wire [7:0] operandAE;
+    wire [7:0] operandBE; //the wire that goes into the aluInputB MUX 
     wire [7:0] aluInputB;
     wire [7:0] aluResultE;
     wire       zero, carry, overflow, negative, sign, parity;
-
-    assign aluInputB = aluSrcE ? imm8E : readData2E;
+    wire [1:0] forwardAE;
+    wire [1:0] forwardBE;
+    
+    assign operandAE = (forwardAE == 2'b00) ? readData1E: 
+                       (forwardAE == 2'b01) ? aluResultM:
+                       (forwardAE == 2'b10) ? writeDataW: imm8M;
+                       
+    assign operandBE = (forwardBE == 2'b00) ? readData2E:
+                       (forwardBE == 2'b01) ? aluResultM:
+                       (forwardBE == 2'b10) ? writeDataW: imm8M;
+                                           
+    assign aluInputB = aluSrcE ? imm8E : operandBE;
 
     alu alu_inst (
-        .a        (readData1E),
+        .a        (operandAE),
         .b        (aluInputB),
         .alu_ctrl (aluControlE),
         .result   (aluResultE),
@@ -171,6 +205,28 @@ module cpu (
         .sign     (sign),
         .parity   (parity)
     );
+    
+    //hazard unit 
+    
+    hazard_unit hu_inst (
+        .readReg1E (readReg1E),
+        .readReg2E (readReg2E),
+        .writeRegM (writeRegM),
+        .writeRegW (writeRegW),
+        .regWriteM (regWriteM),
+        .regWriteW (regWriteW),
+        .regSrcM   (regSrcM),
+        .is_branchE (is_branchE),
+        .instructionD (instructionD),
+        .memReadE (memReadE),
+        .writeRegE (writeRegE),
+        .regWriteE (regWriteE),
+        .forwardAE (forwardAE),
+        .forwardBE (forwardBE),
+        .stall (stall),
+        .flushIFID (flushIFID),
+        .flushIDEX (flushIDEX)
+    );
 
   //branch
     wire eq;
@@ -178,11 +234,11 @@ module cpu (
     wire branch_type;
 
     comparator comp (
-        .readData1 (readData1E),
-        .readData2 (readData2E),
+        .readData1 (operandAE),
+        .readData2 (operandBE),
         .eq        (eq),
         .lt        (lt)
-    );
+    );                 
 
 
     assign branch_type = (branchControlE == 2'b00) ? eq  :
@@ -190,25 +246,17 @@ module cpu (
                          (branchControlE == 2'b10) ? lt  : ~lt;
 
     assign is_branchE = branch_type & branchE;
+    assign pcSrc = is_branchE | jumpD;
+    
+    assign target_addressE = is_branchE ? (pcPlus1E + imm16E) : (pcPlus1D + imm16D); //saves one cycle during jumps
 
-    assign target_addressE = pcPlus1E + imm16E;
+    assign pcNext = (pcSrc) ? target_addressE : pcPlus1;
 
-    assign pcNext = (is_branchE | jumpE) ? target_addressE : pcPlus1;
-
-    // EX/MEM
-    wire [7:0]  readData2M;
-    wire [2:0]  writeRegM;
-    wire [7:0]  aluResultM;
-    wire        regWriteM;
-    wire [7:0]  imm8M;
-    wire [1:0]  regSrcM;
-    wire        memWriteM;
-    wire        memReadM;
 
     EX_MEM ex_mem (
         .clk        (clk),
         .reset      (reset),
-        .readData2E (readData2E),
+        .operandBE  (operandBE),
         .writeRegE  (writeRegE),
         .aluResultE (aluResultE),
         .regWriteE  (regWriteE),
@@ -216,7 +264,7 @@ module cpu (
         .regSrcE    (regSrcE),
         .memWriteE  (memWriteE),
         .memReadE   (memReadE),
-        .readData2M (readData2M),
+        .operandBM  (operandBM),
         .writeRegM  (writeRegM),
         .aluResultM (aluResultM),
         .regWriteM  (regWriteM),
@@ -236,7 +284,7 @@ module cpu (
         .memWrite  (memWriteM),
         .memRead   (memReadM),
         .address   (aluResultM),
-        .writeMem  (readData2M),
+        .writeMem  (operandBM),
         .readMem   (readMemM)
     );
 
