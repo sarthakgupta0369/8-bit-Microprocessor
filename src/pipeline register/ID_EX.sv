ee88eed5b40e5e2d29fbc374d58fde339e14988c
AdKm-2507 <ee250002007@iiti.ac.in>
2026-07-31 17:45:47 +0530

    wires for stack memory, stack pointer and link register added

--- src/pipeline register/ID_EX.sv
+++ src/pipeline register/ID_EX.sv
@@ -12,19 +12,29 @@ module ID_EX (
     input wire  [2:0]  writeRegD, 
     input wire  [2:0]  readReg1D,
     input wire  [2:0]  readReg2D,
+    input wire  [7:0]  spD,
+
     
     //control signals
     input wire         regWriteD,     
     input wire  [3:0]  aluControlD,           
     input wire  [7:0]  imm8D,     
-    input wire  [15:0] imm16D,     
-    input wire         aluSrcD,         
+    input wire  [15:0] imm16D, 
+    input wire         aluSrcAD,    
+    input wire  [1:0]  aluSrcBD,         
     input wire  [1:0]  regSrcD,         
     input wire         memWriteD,
     input wire         memReadD,
     input wire  [1:0]  branchControlD, 
-    input wire         branchD,         
-    input wire         jumpD,
+    input wire         branchD, 
+    input wire         stackReadD,
+    input wire         stackWriteD,
+    input wire         stackSrcD,  
+    input wire         jrjalrD,      
+    input wire [15:0]  readLRD,
+    input wire         spWriteD,
+    input wire         spSrcD,
+    input wire         popWriteD,
     
     //data
     output reg  [15:0] pcPlus1E,
@@ -33,19 +43,28 @@ module ID_EX (
     output reg  [2:0]  writeRegE, 
     output reg  [2:0]  readReg1E,
     output reg  [2:0]  readReg2E,
+    output reg  [7:0]  spE,
     
     //control signals
     output reg         regWriteE,     
     output reg  [3:0]  aluControlE,           
     output reg  [7:0]  imm8E,     
-    output reg  [15:0] imm16E,     
-    output reg         aluSrcE,         
+    output reg  [15:0] imm16E,  
+    output reg         aluSrcAE,   
+    output reg  [1:0]  aluSrcBE,         
     output reg  [1:0]  regSrcE,         
     output reg         memWriteE,
     output reg         memReadE,
     output reg  [1:0]  branchControlE, 
-    output reg         branchE,         
-    output reg         jumpE
+    output reg         branchE,
+    output reg         stackReadE,
+    output reg         stackWriteE,
+    output reg         stackSrcE,
+    output reg         jrjalrE,
+    output reg  [15:0] readLRE,
+    output reg         spWriteE,
+    output reg         spSrcE,
+    output reg         popWriteE
 );
 
     always @(posedge clk) begin
@@ -60,13 +79,22 @@ module ID_EX (
             readReg2E       <= readReg2D;
             imm8E           <= imm8D;
             imm16E          <= imm16D;
-            aluSrcE         <= aluSrcD;
+            aluSrcAE        <= aluSrcAD;
+            aluSrcBE        <= aluSrcBD;
             regSrcE         <= regSrcD;
             memWriteE       <= memWriteD;
             memReadE        <= memReadD;
             branchControlE  <= branchControlD;
             branchE         <= branchD;
-            jumpE           <= jumpD;
+            stackReadE      <= stackReadD;
+            stackWriteE     <= stackWriteD;
+            stackSrcE       <= stackSrcD;
+            spE             <= spD;
+            jrjalrE         <= jrjalrD;
+            readLRE         <= readLRD;
+            spWriteE        <= spWriteD;
+            spSrcE          <= spSrcD;
+            popWriteE       <= popWriteD;
         end 
         else begin
             pcPlus1E        <= 16'd0;
@@ -79,13 +107,22 @@ module ID_EX (
             readReg2E       <= 3'd0;
             imm8E           <= 8'd0;
             imm16E          <= 16'd0;
-            aluSrcE         <= 1'b0;
+            aluSrcAE        <= 1'b0;
+            aluSrcBE        <= 2'd0;
             regSrcE         <= 2'd0;
             memWriteE       <= 1'b0;
             memReadE        <= 1'b0;
             branchControlE  <= 2'd0;
             branchE         <= 1'b0;
-            jumpE           <= 1'b0;
+            stackReadE      <= 1'b0;
+            stackWriteE     <= 1'b0;
+            stackSrcE       <= 1'b0;
+            spE             <= 8'd0;
+            jrjalrE         <= 1'b0;
+            readLRE         <= 16'b0;
+            spWriteE        <= 1'b0;
+            spSrcE          <= 1'b0;
+            popWriteE       <= 1'b0;
         end
     end
 endmodule
