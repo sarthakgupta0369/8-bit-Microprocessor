96f7e4b8bcbf738440d2f19bf0e1e30cff96209f
AdKm-2507 <ee250002007@iiti.ac.in>
2026-07-31 17:48:07 +0530

    wires for stack memory, stack pointer and link register added

--- src/pipeline register/MEM_WB.sv
+++ src/pipeline register/MEM_WB.sv
@@ -8,21 +8,25 @@ module MEM_WB (
     input wire  [2:0]  writeRegM, 
     input wire  [7:0]  aluResultM,
     input wire  [7:0]  readMemM,
+    input wire  [15:0] readStackM,
     
     //control signals
     input wire         regWriteM,         
     input wire  [7:0]  imm8M,     
-    input wire  [1:0]  regSrcM,         
+    input wire  [1:0]  regSrcM,  
+    input wire         popWriteNewNewM,       
 
     //data
     output reg  [2:0]  writeRegW, 
     output reg  [7:0]  aluResultW,
     output reg  [7:0]  readMemW,
+    output reg  [15:0] readStackW,
     
     //control signals
     output reg         regWriteW,       
     output reg  [7:0]  imm8W,      
-    output reg  [1:0]  regSrcW
+    output reg  [1:0]  regSrcW,
+    output reg         popWriteNewNewW
 );
 
     always @(posedge clk) begin
@@ -33,6 +37,8 @@ module MEM_WB (
             regWriteW  <= regWriteM;
             imm8W      <= imm8M;
             regSrcW    <= regSrcM;
+            readStackW <= readStackM;
+            popWriteNewNewW <= popWriteNewNewM;
         end
         else begin
             writeRegW  <= 3'd0;
@@ -41,6 +47,8 @@ module MEM_WB (
             regWriteW  <= 1'b0;
             imm8W      <= 8'd0;
             regSrcW    <= 2'd0;
+            readStackW  <= 16'd0;
+            popWriteNewNewW <= 1'b0;
         end 
     end
 endmodule
