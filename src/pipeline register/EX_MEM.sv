8356add63be183873f77fd59130e0b8fb7a6c605
AdKm-2507 <ee250002007@iiti.ac.in>
2026-07-31 17:47:02 +0530

    wires added for stack memory, stack pointer and link register

--- src/pipeline register/EX_MEM.sv
+++ src/pipeline register/EX_MEM.sv
@@ -8,6 +8,9 @@ module EX_MEM(
     input wire  [7:0]  operandBE,
     input wire  [2:0]  writeRegE, 
     input wire  [7:0]  aluResultE,
+    input wire [15:0]  stackDataE,
+    input wire [7:0]   stackAddrE,
+
     
     //control signals
     input wire         regWriteE,         
@@ -15,18 +18,28 @@ module EX_MEM(
     input wire  [1:0]  regSrcE,         
     input wire         memWriteE,
     input wire         memReadE,
+    input wire         stackReadE,
+    input wire         stackWriteE,
+    input wire         spWriteE,
+    input wire         popWriteNewE,
 
     //data
     output reg  [7:0]  operandBM,
     output reg  [2:0]  writeRegM, 
     output reg  [7:0]  aluResultM,
+    output reg  [15:0] stackDataM,
+    output reg  [7:0]  stackAddrM,
+    output reg         popWriteNewM,
     
     //control signals
     output reg         regWriteM,       
     output reg  [7:0]  imm8M,      
     output reg  [1:0]  regSrcM,         
     output reg         memWriteM,
-    output reg         memReadM
+    output reg         memReadM,
+    output reg         stackReadM,
+    output reg         stackWriteM,
+    output reg         spWriteM
 );
 
     always @(posedge clk) begin
@@ -39,6 +52,12 @@ module EX_MEM(
             regSrcM      <= regSrcE;
             memWriteM    <= memWriteE;
             memReadM     <= memReadE;
+            stackReadM  <= stackReadE;
+            stackWriteM <= stackWriteE;
+            stackDataM  <= stackDataE;
+            stackAddrM  <= stackAddrE;
+            spWriteM    <= spWriteE;
+            popWriteNewM <= popWriteNewE;
         end 
         else begin 
             operandBM    <= 8'd0;
@@ -48,7 +67,13 @@ module EX_MEM(
             imm8M        <= 8'd0;
             regSrcM      <= 2'd0;
             memWriteM    <= 1'b0;
-            memReadM     <= 1'b0;  
+            memReadM     <= 1'b0; 
+            stackReadM   <= 1'b0;
+            stackWriteM  <= 1'b0;
+            stackDataM   <= 16'd0; 
+            stackAddrM   <= 8'd0;
+            spWriteM     <= 1'b0;
+            popWriteNewM <= 1'b0;
         end
     end        
 endmodule
